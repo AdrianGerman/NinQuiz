b0228cfff534220a9902c4cbdc5816317f0805d8
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2022-08-31 11:49:22 -0500

    Se agregarón mas preguntas y el boton "next "

--- app/src/main/java/german/adrian/quiz/MainActivity.kt
+++ app/src/main/java/german/adrian/quiz/MainActivity.kt
@@ -18,8 +18,15 @@ class MainActivity : AppCompatActivity() {
     private val questionBank = listOf(
         Question(R.string.question_zelda, true),
         Question(R.string.question_mario, false),
-        Question(R.string.question_kong, true)
-    )
+        Question(R.string.question_kong, true),
+        Question(R.string.question_year, false),
+        Question(R.string.question_gameboy, true),
+        Question(R.string.question_mano, true),
+        Question(R.string.question_jumpman, true),
+        Question(R.string.question_yoshi, true),
+        Question(R.string.question_nin, false),
+        Question(R.string.question_consola, false),
+        )
 
     private var currentIndex = 0
 
@@ -33,22 +40,56 @@ class MainActivity : AppCompatActivity() {
 
         binding.trueButton.setOnClickListener { view: View ->
             //Toast.makeText(this, R.string.correct_toast,Toast.LENGTH_SHORT).show()
-            val SnackBar = Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_LONG)
-            SnackBar.setBackgroundTint(getColor(R.color.green))
-            SnackBar.show()
+            //val SnackBar = Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_LONG)
+            //SnackBar.setBackgroundTint(getColor(R.color.green))
+            //SnackBar.show()
+            checkAnswer(true)
 
 
 
         }
         binding.falseButton.setOnClickListener { view: View ->
             //Toast.makeText(this, R.string.incorrect_toast,Toast.LENGTH_SHORT).show()
-            val SnackBar = Snackbar.make(view, R.string.incorrect_toast, Snackbar.LENGTH_LONG)
-            SnackBar.setBackgroundTint(getColor(R.color.red))
-            SnackBar.show()
+            //val SnackBar = Snackbar.make(view, R.string.incorrect_toast, Snackbar.LENGTH_LONG)
+            //SnackBar.setBackgroundTint(getColor(R.color.red))
+            //SnackBar.show()
+            checkAnswer(false)
+
+        }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            //val questionTextResId = questionBank[currentIndex] . textResId
+            //binding.questionTextView.setText(questionTextResId)
+            updateQuestion()
         }
 
 
-        val questionTextResId = questionBank[currentIndex].textResId
+        //val questionTextResId = questionBank[currentIndex].textResId
+        //binding.questionTextView.setText(questionTextResId)
+        updateQuestion()
+        }
+
+
+    private fun checkAnswer (userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].respuesta
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT) .show()
+
+    }
+
+
+
+    private fun updateQuestion(){
+        val questionTextResId = questionBank[currentIndex]. textResId
         binding.questionTextView.setText(questionTextResId)
     }
+
+
+
 }
